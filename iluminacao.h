82964205012a9844332551bdab5cda8d2798a58b
Vitor Marques <vitormr97@live.com>
2024-12-08 16:17:44 -0300

    adicionado funcionalidades com mouse e menus, arquivos da textura adicionados

--- iluminacao.h
+++ iluminacao.h
@@ -6,20 +6,21 @@ float anguloLuzX = 0.0;
 float anguloLuzY = 0.0;
 float anguloLuzZ = 0.0;
 
+//variaveis da luz
+GLfloat posicaoLuz[] = {1.0, 1.0, 1.0, 0.0}; // 0: luz direcional 1: pontual
+GLfloat luzAmbiente[] = {0.2, 0.2, 0.2, 1.0}; // rgb + opacidade
+GLfloat luzDifusa[] = {0.8, 0.8, 0.8, 1.0};
+GLfloat luzEspecular[] = {1.0, 1.0, 1.0, 1.0};
+
 void inicializaIluminacao()
 {
-    GLfloat posicaoLuz[] = {1.0, 1.0, 1.0, 0.0}; // 0: luz direcional 1: pontual
+    
     glRotatef(anguloLuzX, 1.0, 0.0, 0.0);
     glRotatef(anguloLuzY, 0.0, 1.0, 0.0);
     glRotatef(anguloLuzZ, 0.0, 0.0, 1.0);
     glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
-    GLfloat luzAmbiente[] = {0.2, 0.2, 0.2, 1.0}; // rgb + opacidade
-    GLfloat luzDifusa[] = {0.8, 0.8, 0.8, 1.0};
-    GLfloat luzEspecular[] = {1.0, 1.0, 1.0, 1.0};
-
-
     glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
     glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular);
-    glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
+    
 }
--- projetoCamera.cpp
+++ projetoCamera.cpp
@@ -1,8 +1,15 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <algorithm>
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
 #include "iluminacao.h"
 
+//variaveis de textura
+GLuint idsTextura[3];
+
+//variaveis de animação
+float velocidadeAnimacao = 0.00;
 
 // partes do braço
 int ombro = 0, cotovelo = 0, mao = 0, dedoMeio = 0, dedoEsq = 0, dedoDir = 0;
@@ -11,21 +18,112 @@ int ombro = 0, cotovelo = 0, mao = 0, dedoMeio = 0, dedoEsq = 0, dedoDir = 0;
 float angulo = 0.0;
 float distancia = 10.0;
 
+void carregarTextura(const char* nomeArquivo, int indice)
+{
+ int largura, altura, canais;
+
+ unsigned char *dados = stbi_load(nomeArquivo, &largura, &altura, &canais, 0);
+
+ if (!dados)
+  exit(1);
+
+ // gerar textura
+ glGenTextures(1, &idsTextura[indice]);
+ glBindTexture(GL_TEXTURE_2D, idsTextura[indice]);
+
+ // configurar parametros da textura
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+ glTexImage2D(GL_TEXTURE_2D, 0, (canais == 4) ? GL_RGBA : GL_RGB, 
+               largura, altura, 0, (canais == 4) ? GL_RGBA : GL_RGB,
+               GL_UNSIGNED_BYTE, dados);
+
+ // liberar a memoria da imagem
+ stbi_image_free(dados);              
+}
+
+void desenhaCubo(float tamanho)
+{
+ // face frontal
+ glBindTexture(GL_TEXTURE_2D, idsTextura[0]);
+ glBegin(GL_QUADS);
+  glTexCoord2f(0.0, 0.0); glVertex3f(-tamanho, -tamanho, tamanho);
+  glTexCoord2f(1.0, 0.0); glVertex3f(tamanho, -tamanho, tamanho);
+  glTexCoord2f(1.0, 1.0); glVertex3f(tamanho, tamanho, tamanho);
+  glTexCoord2f(0.0, 1.0); glVertex3f(-tamanho, tamanho, tamanho);
+ glEnd();
+
+  // face traseira
+  glBindTexture(GL_TEXTURE_2D, idsTextura[0]);
+  glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0); glVertex3f(-tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 0.0); glVertex3f(tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 1.0); glVertex3f(tamanho, tamanho, -tamanho);
+   glTexCoord2f(0.0, 1.0); glVertex3f(-tamanho, tamanho, -tamanho);
+  glEnd();
+
+  // face superior
+  glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+  glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0); glVertex3f(-tamanho, tamanho, -tamanho);
+   glTexCoord2f(1.0, 0.0); glVertex3f(tamanho, tamanho, -tamanho);
+   glTexCoord2f(1.0, 1.0); glVertex3f(tamanho, tamanho, tamanho);
+   glTexCoord2f(0.0, 1.0); glVertex3f(-tamanho, tamanho, tamanho);
+  glEnd();
+
+  // face inferior
+  glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+  glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0); glVertex3f(-tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 0.0); glVertex3f(tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 1.0); glVertex3f(tamanho, -tamanho, tamanho);
+   glTexCoord2f(0.0, 1.0); glVertex3f(-tamanho, -tamanho, tamanho);
+  glEnd();
+
+  // face direita
+  glBindTexture(GL_TEXTURE_2D, idsTextura[2]);
+  glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0); glVertex3f(tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 0.0); glVertex3f(tamanho, tamanho, -tamanho);
+   glTexCoord2f(1.0, 1.0); glVertex3f(tamanho, tamanho, tamanho);
+   glTexCoord2f(0.0, 1.0); glVertex3f(tamanho, -tamanho, tamanho);
+  glEnd();
+
+  // face esquerda
+  glBindTexture(GL_TEXTURE_2D, idsTextura[2]);
+  glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0); glVertex3f(-tamanho, -tamanho, -tamanho);
+   glTexCoord2f(1.0, 0.0); glVertex3f(-tamanho, tamanho, -tamanho);
+   glTexCoord2f(1.0, 1.0); glVertex3f(-tamanho, tamanho, tamanho);
+   glTexCoord2f(0.0, 1.0); glVertex3f(-tamanho, -tamanho, tamanho);
+  glEnd();    
+}
+
+
 void inicializa()
 {
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_TEXTURE_2D);
     glEnable(GL_LIGHTING); // ativa a iluminacao
     glEnable(GL_LIGHT0);
 
     // configuracao da luz
     inicializaIluminacao();
+    carregarTextura("texturas/azulejo.jpg", 0);
+    carregarTextura("texturas/tijolo.png", 1);
+    carregarTextura("texturas/madeira.png", 2);
 }
 
 void display()
 {
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+
 
     float posX = distancia * cos(angulo);
     float posZ = distancia * sin(angulo);
@@ -45,9 +143,9 @@ void display()
     glTranslatef(1.0, 0.0, 0.0);
 
     // Parte superior do braço
-    GLfloat materialVermelho[] = {0.7, 0.0, 0.0, 1.0};
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialVermelho);
+    GLfloat materialVermelho[] = {0.7, 0.0, 0.0, 1.0};;
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialVermelho);
     glScalef(2.0, 0.6, 1.0);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -59,8 +157,8 @@ void display()
 
     // parte inferior do braço
     GLfloat materialVerde[] = {0.0, 0.7, 0.0, 1.0};
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialVerde);
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialVerde);
     glScalef(2.0, 0.4, 1.0);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -72,8 +170,8 @@ void display()
 
     // mao
     GLfloat materialAzul[] = {0.0, 0.0, 1.0, 1.0};
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAzul);
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAzul);
     glScalef(1.0, 0.2, 1.0);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -87,8 +185,8 @@ void display()
 
     // dedo meio
     GLfloat materialAmarelo[] = {1.0, 1.0, 0.0, 1.0};
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
     glScalef(1, 0.2, 0.1);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -98,8 +196,8 @@ void display()
     glTranslatef(0.5, 0.0, 0.40);
 
     // dedo Esq
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
     glScalef(1, 0.2, 0.1);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -110,8 +208,9 @@ void display()
     glTranslatef(0.5, 0.0, -0.40);
 
     // dedo Dir
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
+    
     glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, materialAmarelo);
     glScalef(1, 0.2, 0.1);
     glutSolidCube(1.0);
     glPopMatrix();
@@ -196,6 +295,21 @@ void tecladoEspecial(int tecla, int x, int y)
     }
 }
 
+void mousePressionado(int botao, int estado, int x, int y)
+{
+ if (estado == GLUT_DOWN)
+ {
+  if (botao == GLUT_LEFT_BUTTON){
+   angulo = 0.00;
+   distancia = 10;
+  }
+  else
+    if (botao == GLUT_RIGHT_BUTTON)
+     angulo -= 0.05;
+ }
+ glutPostRedisplay();
+}
+
 void reshape(int largura, int altura)
 {
     glViewport(0, 0, largura, altura);
@@ -209,6 +323,26 @@ void reshape(int largura, int altura)
     glLoadIdentity();
 }
 
+void atualiza(int valor)
+{
+ angulo += velocidadeAnimacao;
+ glutPostRedisplay();
+ glutTimerFunc(16, atualiza, 0);   
+}
+
+
+void menu(int opcao)
+{
+    switch(opcao) {
+        case (1):
+            velocidadeAnimacao = 0.01;
+            break;
+        case (0):
+            velocidadeAnimacao = 0.0;
+            break;
+    }
+} 
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -219,9 +353,19 @@ int main(int argc, char **argv)
     inicializa();
 
     glutDisplayFunc(display);
+    int submenuAnimacao = glutCreateMenu(menu);
+    glutAddMenuEntry("Inicia animacao", 1);
+    glutAddMenuEntry("Pausa animacao", 0);
+
     glutReshapeFunc(reshape);
     glutKeyboardFunc(teclado);
     glutSpecialFunc(tecladoEspecial);
+    glutCreateMenu(menu);
+    glutAddSubMenu("Animacao",submenuAnimacao);
+    glutAttachMenu(GLUT_MIDDLE_BUTTON);
+    glutTimerFunc(1000, atualiza, 0);   
+    glutMouseFunc(mousePressionado);
+
 
     glutMainLoop();
 
